fc6fc0cc7b76d0696492b0ef71cffd56302941e2
Nico Krause <nandi@Nicos-MacBook-Pro.local>
2021-05-07 15:13:07 +0100

    removed public modifier because compiler was complaining

--- contracts/contracts/LeSpaceToken.sol
+++ contracts/contracts/LeSpaceToken.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.21 <0.9.0;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LeSpaceToken is ERC20 {
-    constructor() public ERC20("LeSpaceToken", "LST") {
+    constructor() ERC20("LeSpaceToken", "LST") {
         uint256 oneLST = 1000000000000000000;
         uint256 hundredMillion = 100000000;
         _mint(msg.sender,oneLST*hundredMillion);
